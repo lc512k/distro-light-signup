c4d345e3e2d00e730d40f5e39183c1f52ba3a012
Matt Brennan <mattyb1000@gmail.com>
2016-05-10 12:14:37 +0100

    working nodemon and .env, logging

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import newsletterSignup from '@financial-times/newsletter-signup';
 import express from 'express';
+import logger from 'morgan';
 
 const app = express();
+const port = process.env.PORT || 1337;
 
+app.use(logger(process.env.LOG_FORMAT || (app.get('env') === 'development' ? 'dev' : 'combined')));
 app.use('/signup', newsletterSignup);
 
-app.listen(process.env.PORT || 1337);
+app.listen(port, () => console.log(`Listening on port ${port}`));

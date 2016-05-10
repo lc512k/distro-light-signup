481d304fd23afd65d5a94a1f7b3ebc8057c16099
Matt Brennan <mattyb1000@gmail.com>
2016-05-10 18:07:13 +0100

    signup form from origami, layout, correct font url

--- src/index.js
+++ src/index.js
@@ -3,16 +3,23 @@ import express from 'express';
 import logger from 'morgan';
 import expressHandlebars from 'express-handlebars';
 
+
 import devController from './controllers/dev';
 
 const app = express();
 const port = process.env.PORT || 1337;
 
-app.engine('html', expressHandlebars({}));
+app.engine('html', expressHandlebars({
+	extname: '.html',
+	defaultLayout: 'main'
+}));
 app.set('view engine', 'html');
 
 app.use(logger(process.env.LOG_FORMAT || (app.get('env') === 'development' ? 'dev' : 'combined')));
+
+app.get('/', (req, res) => res.render('signup'));
 app.use('/signup', newsletterSignup);
 app.use('/dev', devController);
+app.use('/public', express.static('public'));
 
 app.listen(port, () => console.log(`Listening on port ${port}`));

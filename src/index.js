cf270cf8c11ff15475baaf0687079d94648577e2
Matt Brennan <mattyb1000@gmail.com>
2016-05-17 14:16:59 +0100

    long stack traces in development

--- src/index.js
+++ src/index.js
@@ -3,15 +3,18 @@ import express from 'express';
 import logger from 'morgan';
 import expressHandlebars from 'express-handlebars';
 
-
 import devController from './controllers/dev';
 
 const app = express();
 const port = process.env.PORT || 1337;
 
+if(app.get('env') !== 'production') {
+	require('longjohn'); // eslint-disable-line global-require
+}
+
 app.engine('html', expressHandlebars({
 	extname: '.html',
-	defaultLayout: 'main'
+	defaultLayout: 'main',
 }));
 app.set('view engine', 'html');
 

40f06b35df3f3edafa48b028d07e8e494823abec
Matt Brennan <mattyb1000@gmail.com>
2016-05-20 11:42:11 +0100

    better form switch, fix querystring parsing

--- client/main.js
+++ client/main.js
@@ -1,7 +1,7 @@
 import oEmailOnlySignup from 'o-email-only-signup';
 import querystring from 'querystring';
 
-const {article} = querystring.parse(location.search);
+const {article} = querystring.parse(location.search.substr(1));
 
 oEmailOnlySignup.init(document.body, {
 	signupUrl: `/signup?article=${article}`,

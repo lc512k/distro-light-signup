86a63aef5df2fc8fc80fd6e3ad0b27cf0b82f4da
Matt Brennan <mattyb1000@gmail.com>
2016-06-06 10:50:36 +0100

    detect facebook android and display a single button instead

--- client/main.js
+++ client/main.js
@@ -1,8 +1,11 @@
 import oEmailOnlySignup from 'o-email-only-signup';
+import url from 'url';
 import querystring from 'querystring';
 
 const {article, product, mailinglist} = querystring.parse(location.search.substr(1));
-
-oEmailOnlySignup.init(document.body, {
-	signupUrl: `/signup?article=${article}&product=${product}&mailinglist=${mailinglist}`,
+const signupUrl = url.format({
+	pathname: '/signup',
+	query: {article, product, mailinglist},
 });
+
+oEmailOnlySignup.init(document.body, {signupUrl});
